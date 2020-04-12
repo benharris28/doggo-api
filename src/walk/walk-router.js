23bf417fceaf7183d9ea3c4fe6da1b10c1e2248e
Ben Harris <benjaminharris28@gmail.com>
2020-04-11 23:44:59 -0400

    added post for new walk. Error on then block needs to be fixed

--- src/walk/walk-router.js
+++ src/walk/walk-router.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path')
 const WalkService = require('./walk-service')
+
 //const { requireAuth } = require('../middleware/jwt-auth')
 
 const walkRouter = express.Router()
+const jsonBodyParser = express.json()
 
 //const serializeWalk = walk => ({
     //id: note.id,
@@ -12,6 +15,73 @@ const walkRouter = express.Router()
     //content: xss(note.content)
 
 //})
+
+walkRouter
+.route('/')
+.get((req, res, next) => {
+    WalkService.getAllWalks(
+        req.app.get('db'),
+
+    )
+    .then(walks => {
+        res.json(walks)
+    })
+    .catch(next)
+})
+.post(jsonBodyParser, (req, res, next) => {
+    const {
+        walker_id,
+        user_id,
+        user_firstname,
+        dog_name,
+        walker_firstname,
+        request_time,
+        walk_date,
+        pickup_address_street_number,
+        pickup_address_street_name,
+        pickup_address_city,
+        pickup_address_province,
+        pickup_address_postal_code,
+        walk_status
+    } = req.body
+    //console.log(req.body)
+    const newWalk = {
+        walker_id,
+        user_id,
+        user_firstname,
+        dog_name,
+        walker_firstname,
+        request_time,
+        walk_date,
+        pickup_address_street_number,
+        pickup_address_street_name,
+        pickup_address_city,
+        pickup_address_province,
+        pickup_address_postal_code,
+        walk_status
+    };
+    console.log(newWalk)
+
+    for (const [key, value] of Object.entries(newWalk))
+      if (value == null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        })
+    
+    
+    WalkService.insertWalk(
+        req.app.get('db'),
+        newWalk
+    )
+    .then(walk => {
+        res
+            .status(201)
+            //.location(path.posix.join(req.originalUrl,`/${walk.walk_id}`))
+            .json(WalkService.serializeWalk(walk))
+
+    })
+    .catch(next)
+})
 
 walkRouter
 .route('/:walk_id')
--- src/walk/walk-service.js
+++ src/walk/walk-service.js
@@ -14,7 +14,47 @@ const WalkService = {
             .select('*')
             .where('user_id', user_id)
 
+    },
+    insertWalk(db, newWalk) {
+        return db
+            .insert(newWalk)
+            .into('walks')
+            .returning('*')
+            .then(([walk]) => walk)
+            .then(walk => {
+                WalkService.getWalkById(db, walk.walk_id)
+            })
+    },
+    updateWalk(db, walk_id, newWalkStatus) {
+        return db('walks')
+            .where({ walk_id })
+            .update(newWalkStatus)
+
+    },
+    getAllWalks(db) {
+        return db
+            .from('walks')
+            .select('*')
+            
+},
+    serializeWalk(walk) {
+    return {
+        
+        walker_id: walk.walker_id,
+        user_id: walk.user_id,
+        user_firstname: walk.user_firstname,
+        dog_name: walk.dog_name,
+        walker_firstname: walk.walker_firstname,
+        request_time: walk.request_time,
+        walk_date: walk.walk_date,
+        pickup_address_street_number: walk.pickup_address_street_number,
+        pickup_address_street_name: xss(walk.pickup_address_street_name),
+        pickup_address_city: xss(walk.pickup_address_city),
+        pickup_address_province: xss(walk.pickup_address_province),
+        pickup_address_postal_code: xss(walk.pickup_address_postal_code),
+        walk_status: xss(walk.walk_status)
     }
+}
 }
 
 module.exports = WalkService
