11858f5cfa20f408ab40dea8b6e971bf0c6fc47c
Ben Harris <benjaminharris28@gmail.com>
2020-04-25 13:13:20 -0400

    updated test

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -206,9 +206,10 @@ function seedUsers(db, users) {
     ...user,
     password: bcrypt.hashSync(user.password, 1)
   }))
+  console.log(preppedUsers)
   return db.into('users').insert(preppedUsers)
-    .then(() =>
-      // update the auto sequence to stay in sync
+    .then(() => 
+       //update the auto sequence to stay in sync
       db.raw(
         `SELECT setval('users_id_seq', ?)`,
         [users[users.length - 1].user_id],
